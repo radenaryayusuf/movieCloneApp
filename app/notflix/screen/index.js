e637b8f9707beb7112a476393819d4a6880a959f
babaiyu <bayuputra9a@gmail.com>
2018-12-17 10:16:12 +0700

    Index
    Index new

--- app/notflix/screen/index.js
+++ app/notflix/screen/index.js
@@ -3,9 +3,13 @@ import MoviePage from './MoviePage'
 import SearchPage from './SearchPage'
 import CastPage from './CastPage'
 
+import LoginPage from './LoginPage'
+
 export {
     HomePage,
     MoviePage,
     SearchPage,
-    CastPage
+    CastPage,
+
+    LoginPage,
 }
